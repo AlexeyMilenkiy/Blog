7cef83365558829ff19e8810231fdefa2de5b89f
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2019-09-25 21:48:57 +0300

    fixes show posts

--- client/src/components/FriendsPosts/friends-posts.component.ts
+++ client/src/components/FriendsPosts/friends-posts.component.ts
@@ -12,20 +12,37 @@ export class FriendsPostsComponent implements OnInit {
   friendsPosts: Post[] = [];
   isError: boolean = false;
   isEmpty: boolean = false;
+  isSort: boolean = false;
+  isIncrease: boolean = true;
 
   constructor(private postService: PostService) {}
+
+  sortArray() {
+    this.isIncrease = !this.isIncrease;
+    if(this.isIncrease) {
+      this.friendsPosts.sort((a,b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
+    } else {
+      this.friendsPosts.sort((a,b) => (a.date > b.date) ? -1 : ((b.date > a.date) ? 0 : -1));
+    }
+  }
+
 
   ngOnInit(): void {
     const userId = parseInt(localStorage.getItem('id'));
     this.postService.getMyFriendsPosts(userId)
       .subscribe(posts => {
-        if(posts.length){
-          this.isEmpty = false;
-          this.friendsPosts = [...posts];
-        } else  {
-          this.isEmpty = true;
+          if (posts.length) {
+            this.isSort = posts.length !== 1;
+            this.isEmpty = false;
+            posts.sort((a, b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
+            this.friendsPosts = [...posts];
+          } else {
+            this.isEmpty = true;
           }
-      },
-        (error => this.isError = true));
+        },
+        (error => {
+          if (error) this.isError = true
+        })
+      );
   }
 }
