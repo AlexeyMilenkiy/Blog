c37746e71e66d528bd5c6ea4647679f9a4bfa823
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2019-09-27 21:16:21 +0300

    fixes

--- server/helpers/validator.js
+++ server/helpers/validator.js
@@ -1,4 +1,4 @@
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 
 const validate = validations => {
     return async (req, res, next) => {
