2d83de8cca3b187f309fa9cb0a2186d886e41467
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2019-09-29 23:44:24 +0300

    fixes

--- server/db/config.js
+++ server/db/config.js
@@ -1,7 +1,7 @@
 module.exports = [
-    'database_name',      // 'The name of the database'
-    'user_name',          // 'The username which is used to authenticate against the database'
-    'user_password',      // 'The password which is used to authenticate against the database'
+    'databse_name',  // 'The name of the database'
+    'user_name',     // 'The username which is used to authenticate against the database'
+    'password',      // 'The password which is used to authenticate against the database'
     {
         host: 'localhost',
         dialect: 'postgres',
