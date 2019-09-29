ba471ea1feeb7c3ce93414899d1ca33af61d6d7a
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2019-09-29 23:44:53 +0300

    fixes

--- server/db/config.js
+++ server/db/config.js
@@ -1,5 +1,5 @@
 module.exports = [
-    'databse_name',  // 'The name of the database'
+    'database_name',  // 'The name of the database'
     'user_name',     // 'The username which is used to authenticate against the database'
     'password',      // 'The password which is used to authenticate against the database'
     {
