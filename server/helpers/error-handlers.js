0e55897dc30bff4552bd642da56d14b46753663b
Alexey <alexey@MacBook-Pro-Alexey.local>
2019-09-30 17:07:03 +0300

    added error handler

---
+++ server/helpers/error-handlers.js
@@ -0,0 +1,8 @@
+function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500);
+    res.render('error', { error: err })
+}
+module.exports = errorHandler;
